c4044efe04edcc1f3c82c9bab3722e06a3cd0e78
Hazel Paiz <paiz.hazel@gmail.com>
2019-12-11 23:34:47 -0800

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Hello and welcome to my site!</h1>
+    </header>
+);
+
+// we need to export Header, otherwise
+// other files have no idea what Header
+export default Header;
